a2001c943aab0cd606e9c668b7332769a4a7ac28
Jigar Parekh <jparekh2@icloud.com>
2018-07-30 17:32:24 +0530

    Initialized index.php with basic form

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<title>Login Page</title>
+
+<link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body style="background-color:#2980b9">
+		<div id="main-wrapper">
+			<center>
+				<h2>Login Form</h2>	
+			</center>
+
+			<form class="myform" action="index.php" method="post">
+			<label>Username: </label><br>
+			<input type="text" class="inputvalues" placeholder="Type your username"/><br>
+			<label>Password: </label><br>
+			<input type="password" class="inputvalues" placeholder="Type your password"/><br>
+			<input type="submit" id="login_btn" value="Login"/><br>
+			<input type="button" id="register_btn" value="Register"/>
+		</form>
+
+		</div>
+</body>
+
+</html>
