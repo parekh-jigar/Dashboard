6b066c9f05a8617dc5f5d5416c514cba1d808a2f
Jigar Parekh <jparekh2@icloud.com>
2018-08-01 16:45:45 +0530

    Added homepage.php

---
+++ homepage.php
@@ -0,0 +1,33 @@
+ <?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<title>Home Page</title>
+
+<link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body style="background-color:#95a5a6">
+		<div id="main-wrapper">
+			<center>
+				<h2>Login Form</h2>
+				<h3>Welcome User</h3>	
+				<img src="images/avatar.png" class="avatar"/>
+			</center>
+
+			<form class="myform" action="homepage.php" method="post">
+			
+				<input type="button" id="logout_btn" value="Log out"/>
+			
+			</form>
+
+		</div>
+</body>
+
+</html>
