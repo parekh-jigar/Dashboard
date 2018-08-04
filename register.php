2c471b4626f78ccaf8ef7472244e26748e1b99d5
Jigar Parekh <jparekh2@icloud.com>
2018-08-04 20:08:18 +0530

    DB Configuration

--- register.php
+++ register.php
@@ -44,6 +44,28 @@
 					{
 						$query= "select * from user WHERE username='$username'";
 						$query_run = mysqli_query($con,$query);
+
+						if(mysqli_num_rows($query_run)>0)
+						{
+							// There is already a user with the same username
+							echo '<script type="text/javascript"> alert("Username already exists, try another username") </script>';
+						}
+						{
+							$query= "insert into user values('$username','$password')";
+							$query_run= mysqli_query($con,$query);
+							if($query_run)
+							{
+								echo '<script type="text/javascript"> alert("User registered. Go to login page") </script>';
+							}
+							else
+							{
+								echo '<script type="text/javascript"> alert("Error!") </script>';
+							}
+						}
+					}
+					else
+					{
+						echo '<script type="text/javascript"> alert("Both the passwords are different") </script>';
 					}
 				}
 
