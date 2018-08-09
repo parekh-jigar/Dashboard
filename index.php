036b95d081fb4ae8d2a5acc7dcc8467ffefe2240
Jigar Parekh <jparekh2@icloud.com>
2018-08-09 23:46:20 +0530

    DB config for login page, and updated the form tag

--- index.php
+++ index.php
@@ -22,12 +22,39 @@
 
 			<form class="myform" action="index.php" method="post">
 				<label>Username: </label><br>
-				<input type="text" class="inputvalues" placeholder="Type your username"/><br>
+				<input name="username" type="text" class="inputvalues" placeholder="Type your username" required/><br>
 				<label>Password: </label><br>
-				<input type="password" class="inputvalues" placeholder="Type your password"/><br>
-				<input type="submit" id="login_btn" value="Login"/><br>
+				<input name="password" type="password" class="inputvalues" placeholder="Type your password" required/><br>
+				<input name="login" type="submit" id="login_btn" value="Login"/><br>
 				<a href= "register.php"><input type="button" id="register_btn" value="Register"/></a>
 			</form>
+
+			<?php
+			if(isset($_POST['login']))
+			{
+				$username= $_POST['username'];
+				$password= $_POST['passowrd'];
+
+				$query= "select * from user WHERE username='$username' AND password='$password'";
+
+				$query_run = mysqli_query($con, $query);
+
+				if(mysqli_num_rows($query_run)>0)
+				{
+					// Valid
+					$_SESSION['username']= $username;
+					header('location:homepage.php');
+
+				}
+				else
+				{
+					// Invalid
+					echo '<script type="text/javascript"> alert("Invalid credentials") </script>';
+				}
+
+			}
+
+			?>
 
 		</div>
 </body>
