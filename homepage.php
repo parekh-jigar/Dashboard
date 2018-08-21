b6ad773b03a9f8101fefea7abf9483fafe9185d7
Jigar Parekh <jparekh2@icloud.com>
2018-08-21 23:53:20 +0530

    Added div classes

--- homepage.php
+++ homepage.php
@@ -3,10 +3,10 @@ session_start();
 require_once('dbconfig/config.php');
 
 if(isset($_POST['logout']))
-		{
-			session_destroy();
-			header('Location: index.php');
-		}
+{
+	session_destroy();
+	header('Location: index.php');
+}
 
 ?>
 
@@ -22,21 +22,25 @@ if(isset($_POST['logout']))
 </head>
 
 <body style="background-color:#95a5a6">
-	<div class="container-homepage100">
-		<center>
-			<h2>Login Form</h2>
-			<h3>Welcome 
-				<?php echo $_SESSION['username'] ?>
-			</h3>	
-			<img src="images/avatar.png" class="avatar"/>
-		</center>
+	<div class="limiter">
+		<div class="container-homepage100">
+			<div class="wrap-homepage100">
+				<center>
+					<h2>Login Form</h2>
+					<h3>Welcome 
+						<?php echo $_SESSION['username'] ?>
+					</h3>	
+					<img src="images/avatar.png" class="avatar"/>
+				</center>
 
-		<form class="myform" action="" method="post">
-			
-			<input name="logout" type="submit" id="logout_btn" value="Log out"/>
-			
-		</form>
+				<form class="myform" action="" method="post">
 
+					<input name="logout" type="submit" id="logout_btn" value="Log out"/>
+
+				</form>
+
+			</div>
+		</div>
 	</div>
 </body>
 
