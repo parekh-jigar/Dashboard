a912634736f96db1999cd65591a41c37eb347332
Jigar Parekh <jparekh2@icloud.com>
2018-08-29 21:19:48 +0530

    Added src for bootstrap elements

--- register.php
+++ register.php
@@ -8,8 +8,12 @@ require "dbconfig/config.php";
 <head>
 
 	<title>Registration Page</title>
-
 	<link rel="stylesheet" href="css/register.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 </head>
 
